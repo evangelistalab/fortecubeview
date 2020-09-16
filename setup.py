46ba1ee1c49f0764018c3ce1ff00c5a2daa95429
Francesco Evangelista <francesco.evangelista@gmail.com>
2020-09-16 13:44:41 -0400

    Update to v.0.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fortecubeview",
-    version="0.2.1",
+    version="0.2.2",
     author="Francesco Evangelista",
     author_email="",
     description="A geometry, cube file, and vibrational modes viewer for Psi4 and Forte",
